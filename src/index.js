21f6a1a47682fd0750e0e8fe0c4ce324cab41f1d
tactik8 <admin@tactik8.com>
2026-06-04 10:06:49 -0400

    go

--- src/index.js
+++ src/index.js
@@ -6,8 +6,34 @@ import { testRecords } from './testRecords/testRecords.js'
 export const htmlHelpers = {
 
     classElements: classElements,
-    testRecords: testRecords
+    testRecords: testRecords,
+    formatRecord: formatrecord
 
 }
+
+
+function formatRecord(value, baseUrl){
+
+    if(Array.isArray(value)){
+        return value.map(x => formatRecord(x, baseUrl))
+    }
+
+    if(value?.['@type'] || valeu?.['@id']){
+
+        let url = new URL(encodeURIComponent("/" + value?.['@idb'] || ""), baseUrl)
+
+        value['@id'] = `<a href="${url}">${value?.['@id'] || "na"}</a>`
+
+        for(let k of Object.keys(value)){
+            value[k] = formatRecord(value[k], baseUrl)
+        }
+
+        return value
+    }
+
+    return value
+
+}
+
 
 export default { htmlHelpers }
